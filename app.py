061703630eba0bb3493e204e7183c60e135c755d
The Normal Juan <juanpabloverdier@gmail.com>
2024-06-10 23:41:16 -0300

    Si yo fuera presidente, la Rosada sería un boliche

--- app.py
+++ app.py
@@ -1,22 +1,19 @@
 from flask import Flask, render_template, jsonify, request
-from dotenv import load_dotenv
-
-import os
 import mercadopago
 
+# Credenciales de MercadoPago
+YOUR_PUBLIC_KEY = "APP_USR-3f9b570c-0a48-427b-a9bd-f82cc714d9e7"
+YOUR_ACCESS_TOKEN = "APP_USR-6830449923842012-051321-4794b782a5fe37b99163a88cb9bb11af-171201186"
 
 app = Flask(__name__)
-sdk = mercadopago.SDK(os.getenv('YOUR_ACCESS_TOKEN'))
-
+sdk = mercadopago.SDK(YOUR_ACCESS_TOKEN)
 
 @app.route('/')
 def home():
-   return render_template('index.html', public_key=os.getenv('YOUR_PUBLIC_KEY'))
-if __name__ == '__main__':
-   app.run()
+    return render_template('index.html', public_key=YOUR_PUBLIC_KEY)
 
 @app.route('/process_payment', methods=['POST'])
-def add_income():
+def process_payment():
     request_values = request.get_json()
     
     payment_data = {
@@ -24,7 +21,7 @@ def add_income():
         "token": request_values["token"],
         "installments": int(request_values["installments"]),
         "payment_method_id": request_values["payment_method_id"],
-        "issuer_id": request_values["issuer_id"],
+        "issuer_id": request_values.get("issuer_id"),
         "payer": {
             "email": request_values["payer"]["email"],
             "identification": {
@@ -37,9 +34,7 @@ def add_income():
     payment_response = sdk.payment().create(payment_data)
     payment = payment_response["response"]
 
-    print("status =>", payment["status"])
-    print("status_detail =>", payment["status_detail"])
-    print("id=>", payment["id"])
+    return jsonify(payment), payment_response["status"]
 
-    return jsonify(payment), 200
-
+if __name__ == '__main__':
+    app.run(debug=True)
